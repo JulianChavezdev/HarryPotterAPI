6a87e24903da14b3bd08cc1324bac13782c49bf8
ElCapidev <julicgavez1@gmail.com>
2025-12-17 13:20:40 +0100

    adding js

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('potter-form');
+const nameInput = document.getElementById('potter-name');
+const resultDiv = document.getElementById('potter-info');
+
+async function fetchPotterData(characterName){
+    
+}
